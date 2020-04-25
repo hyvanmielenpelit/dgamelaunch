eb5c682e26f7b079d70485bf222cef668bbb4aed
TommiGustafsson-HMP <tommi.gustafsson@hyvanmielenpelit.fi>
2020-04-25 13:29:23 -0400

    Added automatic renaming of server address.

--- create-new-gnollhack-version.sh
+++ create-new-gnollhack-version.sh
@@ -18,6 +18,14 @@ sudo cp examples/*.txt /opt/gnollhack/server.gnollhack.com/
 echo "Copying dgamelaunch.conf"
 sudo cp examples/dgamelaunch.conf /opt/gnollhack/server.gnollhack.com/etc
 
+if [ $# -eq 2 ]; then
+  echo "Modifying the server address in dgamelaunch.conf to $2-server.gnollhack.com"
+  sudo sed -i -r 's/$ATTR\(14\)server.gnollhack.com/$ATTR\(14\)$2-server.gnollhack.com/g' /opt/gnollhack/server.gnollhack.com/etc/dgamelaunch.conf
+else
+  echo "The server address in dgamelaunch.conf was not modified, since the second argument was not given."
+  echo "If you want to change the server address automatically, the second argument needs to be, for example, 'eu'."
+fi
+
 echo "Copying default rc files"
 sudo cp ./dgl-default-rcfile /opt/gnollhack/server.gnollhack.com/dgl-default-rcfile.gnh
 sudo cp ./dgl-default-curses-putty-rcfile /opt/gnollhack/server.gnollhack.com/dgl-default-curses-putty-rcfile.gnh
