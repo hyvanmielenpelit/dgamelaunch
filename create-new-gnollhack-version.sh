811132dc04f106697a050d9f97e9c18614a1fcf7
TommiGustafsson-HMP <tommi.gustafsson@hyvanmielenpelit.fi>
2020-05-03 09:08:26 -0400

    Add deletion of old options files.

--- create-new-gnollhack-version.sh
+++ create-new-gnollhack-version.sh
@@ -5,6 +5,11 @@ if [ $# -eq 0 ]; then
   exit 1
 fi
 
+if [ $# -eq 1 ]; then
+  echo "Only one argument. The second argument must be the short name of the GnollHack game. For example, gnollhack5."
+  exit 1
+fi
+
 if test -f /$1/gnollhack; then
   echo "GnollHack found in /$1/"
 else
@@ -18,9 +23,9 @@ sudo cp examples/*.txt /opt/gnollhack/server.gnollhack.com/
 echo "Copying dgamelaunch.conf"
 sudo cp examples/dgamelaunch.conf /opt/gnollhack/server.gnollhack.com/etc
 
-if [ $# -eq 2 ]; then
-  echo "Modifying the server address in dgamelaunch.conf to $2-server.gnollhack.com"
-  sudo sed -i -r "s/ATTR\(14\)server.gnollhack.com/ATTR\(14\)$2-server.gnollhack.com/g" /opt/gnollhack/server.gnollhack.com/etc/dgamelaunch.conf
+if [ $# -eq 3 ]; then
+  echo "Modifying the server address in dgamelaunch.conf to $3-server.gnollhack.com"
+  sudo sed -i -r "s/ATTR\(14\)server.gnollhack.com/ATTR\(14\)$3-server.gnollhack.com/g" /opt/gnollhack/server.gnollhack.com/etc/dgamelaunch.conf
 else
   echo "The server address in dgamelaunch.conf was not modified, since the second argument was not given."
   echo "If you want to change the server address automatically, the second argument needs to be, for example, 'eu'."
@@ -29,15 +34,18 @@ fi
 if [ -d "/opt/gnollhack/server.gnollhack.com/$1/" ]; then
     echo "Directory /opt/gnollhack/server.gnollhack.com/$1 already exists."
 else
-  echo "Creating /opt/gnollhack/server.gnollhack.com/$1 and giving permissions"
+  echo "Creating /opt/gnollhack/server.gnollhack.com/$1 and giving permissions."
   sudo mkdir /opt/gnollhack/server.gnollhack.com/$1
   sudo chmod a+rwx /opt/gnollhack/server.gnollhack.com/$1
 fi
 
-echo "Copying default rc files"
+echo "Copying default rc files."
 sudo cp ./dgl-default-rcfile /opt/gnollhack/server.gnollhack.com/$1/dgl-default-rcfile.gnh
 sudo cp ./dgl-default-curses-putty-rcfile /opt/gnollhack/server.gnollhack.com/$1/dgl-default-curses-putty-rcfile.gnh
 sudo cp ./dgl-default-curses-ssh-rcfile /opt/gnollhack/server.gnollhack.com/$1/dgl-default-curses-ssh-rcfile.gnh
+
+echo "Deleting the old options files from users, if any."
+sudo rm -r /opt/gnollhack/server.gnollhack.com/dgldir/userdata/*_$2.gnhrc
 
 shopt -s nullglob dotglob
 varfiles=(/opt/gnollhack/server.gnollhack.com/$1/var/*)
